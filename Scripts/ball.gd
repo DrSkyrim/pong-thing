fd2d61b38a3567945007748f67b09ab58220b8f0
Martin Lukas <90237322+DrSkyrim@users.noreply.github.com>
2025-11-20 19:22:40 +0200

    Bruh code

---
+++ Scripts/ball.gd
@@ -0,0 +1,8 @@
+extends CharacterBody2D
+
+
+func _physics_process(delta: float) -> void:
+		velocity = Vector2(200, 200)  # starting speed
+		var collision = move_and_collide(velocity * delta)
+		if collision:
+			velocity = velocity.bounce(collision.get_normal())
