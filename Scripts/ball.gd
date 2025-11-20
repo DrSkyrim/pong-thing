2d656bff69481e99572885daef7302791c5bb6d6
Martin Lukas <90237322+DrSkyrim@users.noreply.github.com>
2025-11-20 19:34:48 +0200

    Fix bouncing

--- Scripts/ball.gd
+++ Scripts/ball.gd
@@ -5,4 +5,5 @@ func _physics_process(delta: float) -> void:
 		velocity = Vector2(200, 200)  # starting speed
 		var collision = move_and_collide(velocity * delta)
 		if collision:
+			print("Collided")
 			velocity = velocity.bounce(collision.get_normal())
